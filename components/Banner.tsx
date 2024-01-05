924f079e2bd245d9fdf4f0e7d165bd32f9629a86
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2024-01-05 17:01:44 +0600

    change background color

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ export const Banner = () => {
   return (
     <section
       id="home"
-      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col md:flex-row-reverse justify-center items-center gap-4 lgl:gap-8 mdl:gap-10 xl:gap-4 text-center"
+      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col md:flex-row-reverse justify-center items-center gap-4 lgl:gap-8 mdl:gap-10 xl:gap-4"
     >
       <motion.div
         initial={{ y: 10, opacity: 0 }}
@@ -44,7 +44,7 @@ export const Banner = () => {
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.9 }}
-          className="text-base mdl:max-w-[650px] font-medium text-textDark mx-auto my-5 "
+          className="text-base mdl:max-w-[650px] font-medium text-textDark my-5 "
         >
           Proficient in HTML, CSS, and JavaScript, I am always exploring new
           frameworks and libraries to stay at the forefront of front-end
